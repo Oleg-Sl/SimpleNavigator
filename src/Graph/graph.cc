15a38d1d9471fbfe6a6a2540087036a3cea55aee
staunton <staunton@student.21-school.ru>
2024-01-03 18:21:56 +0700

    Load from file is done

--- src/Graph/graph.cc
+++ src/Graph/graph.cc
@@ -1,26 +1,89 @@
+//#define _CRT_SECURE_NO_WARNINGS
 #include "graph.h"
 
 using namespace s21;
 
 s21::Graph::Graph(size_t size) {
 	size_ = size;
-	std::vector<size_t> row(size);
-	matrix_ = std::vector<std::vector<size_t>> (size, row);
+	SetEmptyMatrix(size);
 }
 
 void s21::Graph::SetValue(size_t row, size_t column, size_t value) {
 	if (row >= size_ || column >= size_) {
 		throw std::out_of_range("Cell index is out of range");
 	}
-
-	if (value <= 0) {
+	if (value < 0) {
 		throw std::invalid_argument("Value must be a natural number");
 	}
 	matrix_[row][column] = value;
 }
 
+size_t s21::Graph::GetValue(size_t row, size_t column) {
+	if (row >= size_ || column >= size_) {
+		throw std::out_of_range("Cell index is out of range");
+	}
+	return matrix_[row][column];
+}
+
+size_t s21::Graph::GetSize() {
+	return size_;
+}
+
 void Graph::LoadGraphFromFile(std::string filename) {
+	std::ifstream in;
+	in.open(filename);
+	if (in.is_open()) {
+		std::string line;
+		bool first_line = true;
+		size_t row = 0;
+		try {
+			while (std::getline(in, line)) {
+				if (first_line) {
+					ParseSize(line);
+					first_line = false;
+				}
+				else {
+					ParseLine(line, row);
+					++row;
+				}
+			}
+		}
+		catch (const std::exception& ex) {
+			std::cout << ex.what();
+		}
+	}
+	in.close();
 }
 
 void Graph::ExportGraphToDot(std::string filename) {
+}
+
+void s21::Graph::ParseSize(std::string line) {
+	try {
+		int size = std::stoi(line);
+		if (size == 0) throw std::invalid_argument("The matrix has size zero");
+		if (size < 0) throw std::invalid_argument("The matrix has incorrect size");
+		size_ = size;
+		SetEmptyMatrix(size);
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what();
+	}
+}
+
+void s21::Graph::ParseLine(std::string line, size_t row) {
+	size_t column = 0;
+	char* token = strtok(const_cast<char*>(line.c_str()), " ");
+	while (token != nullptr) {
+		SetValue(row, column, std::stoi(token));
+		token = strtok(nullptr, " ");
+		++column;
+	}
+}
+
+void s21::Graph::SetEmptyMatrix(size_t size) {
+	if (size > 0) {
+		std::vector<size_t> row(size);
+		matrix_ = std::vector<std::vector<size_t>>(size, row);
+	}
 }
--- src/Graph/graph.h
+++ src/Graph/graph.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 #include <stdexcept>
+#include <fstream>
+#include <iostream>
 
 namespace s21 {
 
@@ -12,11 +14,16 @@ public:
 	Graph() = default;
 	Graph(size_t size);
 	void SetValue(size_t row, size_t column, size_t value);
+	size_t GetValue(size_t row, size_t column);
+	size_t GetSize();
 	void LoadGraphFromFile(std::string filename);
 	void ExportGraphToDot(std::string filename);
 private:
 	std::vector<std::vector<size_t>> matrix_{ 0 }; /** <data matrix> */
 	size_t size_ = 0;
+	void ParseSize(std::string line);
+	void ParseLine(std::string line, size_t row);
+	void SetEmptyMatrix(size_t size);
 };
 
 } // namespace s21
