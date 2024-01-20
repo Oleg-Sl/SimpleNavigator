99f544f4c5ee8b5ed8e03583984f4bfe744d607e
Alexander <varlybot@student.21-school.ru>
2024-01-20 14:25:11 +0700

    added tests

---
+++ src/tests/common_tests.h
@@ -0,0 +1,12 @@
+#ifndef SIMPLE_NAVIGATOR_TESTS_TESTS_H
+#define SIMPLE_NAVIGATOR_TESTS_TESTS_H
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+namespace s21 {
+const std::string kAssetsDir = std::string(ASSETS_DIR) + "/";
+}
+
+#endif  // SIMPLE_NAVIGATOR_TESTS_TESTS_H
--- src/tests/simulated_annealing_test.cc
+++ src/tests/simulated_annealing_test.cc
@@ -1,13 +1,9 @@
-#include <gtest/gtest.h>
-
-#include <string>
-
 #include "../Model/s21_graph.h"
 #include "../Model/s21_graph_algorithms.h"
-
-std::string kAssetsDir = std::string(ASSETS_DIR) + "/";
+#include "common_tests.h"
 
 namespace s21 {
+
 TEST(SimulatedAnnealing, SingleVertexGraph) {
   Graph graph;
   graph.LoadGraphFromFile(kAssetsDir + "graph_1x1.txt");
@@ -31,24 +27,25 @@ TEST(SimulatedAnnealing, TwoVertexGraph) {
   ASSERT_EQ(result.distance, 4);
 }
 
-// TEST(SimulatedAnnealing, LargeGraph) {
-//   Graph graph;
-//   graph.LoadGraphFromFile(kAssetsDir + "graph_example.txt");
-//   GraphAlgorithms algorithms;
-//   TsmResult result =
-//       algorithms.SolveTravelingSalesmanProblemSimulatedAnnealing(graph);
+TEST(SimulatedAnnealing, LargeGraph) {
+  Graph graph;
+  graph.LoadGraphFromFile(kAssetsDir + "graph_example.txt");
+  GraphAlgorithms algorithms;
+  TsmResult result =
+      algorithms.SolveTravelingSalesmanProblemSimulatedAnnealing(graph);
 
-//   ASSERT_EQ(result.vertices.size(), graph.GetSize() + 1);
-//   ASSERT_LT(result.distance, 260);
-// }
+  ASSERT_EQ(result.vertices.size(), graph.GetSize() + 1);
+  ASSERT_LT(result.distance, 260);
+}
 
-// TEST(SimulatedAnnealing, ThrowNoSolutionGraph) {
-//   Graph graph;
-//   graph.LoadGraphFromFile(kAssetsDir + "graph_no_solution.txt");
-//   GraphAlgorithms algorithms;
-//   ASSERT_THROW(
-//       algorithms.SolveTravelingSalesmanProblemSimulatedAnnealing(graph),
-//       std::invalid_argument);
-// }
+TEST(SimulatedAnnealing, NoSolutionGraph) {
+  Graph graph;
+  graph.LoadGraphFromFile(kAssetsDir + "graph_no_solution.txt");
+  GraphAlgorithms algorithms;
+  TsmResult result =
+      algorithms.SolveTravelingSalesmanProblemSimulatedAnnealing(graph);
+
+  ASSERT_EQ(std::numeric_limits<double>::infinity(), result.distance);
+}
 
 }  // namespace s21
